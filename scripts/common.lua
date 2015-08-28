fa18457a9d939ad67b3994b098c746daf9932008
Coranna Howard <me@komiga.com>
2015-08-28 16:50:51 -0400

    scripts/common: correct togo/scripts/common import under premake.

--- scripts/common.lua
+++ scripts/common.lua
@@ -1,5 +1,10 @@
 
-dofile("../dep/togo/scripts/common.lua")
+-- FIXME: u g h, why
+if precore then
+	dofile("../dep/togo/scripts/common.lua")
+else
+	dofile("dep/togo/scripts/common.lua")
+end
 
 function quanta_libs()
 	return {

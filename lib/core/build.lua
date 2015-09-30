84db879a00553c7883716bd88477235ce2e1b2b8
Coranna Howard <me@komiga.com>
2015-09-30 17:53:56 -0400

    lib/core/build: added luajit2.dep.

--- lib/core/build.lua
+++ lib/core/build.lua
@@ -1,12 +1,29 @@
 
 local S, G, R = precore.helpers()
 
+precore.make_config("quanta.lib.core.luajit2.dep", nil, {
+{project = function(p)
+	configuration {}
+		includedirs {
+			G"${DEP_PATH}/luajit/include/",
+		}
+
+	if not precore.env_project()["NO_LINK"] then
+		libdirs {
+			G"${DEP_PATH}/luajit/lib/",
+		}
+		links {
+			":libluajit-5.1.a",
+		}
+	end
+end}})
+
 precore.make_config("quanta.lib.core.dep", {
 	reverse = true,
 }, {
 "quanta.base",
 "togo.lib.core.dep",
--- "quanta.lib.core.luajit2.dep",
+"quanta.lib.core.luajit2.dep",
 {project = function(p)
 	quanta.library_config("core")
 end}})

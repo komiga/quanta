224fea54f96812da5cd38080ec91f571907a6dc1
Coranna Howard <me@komiga.com>
2016-02-01 17:53:05 -0500

    lib/core/test/composition/general.lua: added instance selection tests; scope tests; simplified output; tidy.

--- lib/core/test/composition/general.lua
+++ lib/core/test/composition/general.lua
@@ -16,138 +16,174 @@ function make_test(text, measurement, items)
 end
 
 local translation_tests = {
-	make_test(
-		"x",
-		Measurement(), {
-		make_instance("x", nil, 0, 0, true, true, true, true, {}, {}, {}),
-	}),
-	make_test(
-		"x$1",
-		Measurement(), {
-		make_instance("x", nil, 1, 0, true, true, true, true, {}, {}, {}),
-	}),
-	make_test(
-		"x$1$2",
-		Measurement(), {
-		make_instance("x", nil, 1, 2, true, true, true, true, {}, {}, {}),
-	}),
-	make_test(
-		"{x, y}",
-		Measurement(), {
-		make_instance("x", nil, 0, 0, true, true, true, true, {}, {}, {}),
-		make_instance("y", nil, 0, 0, true, true, true, true, {}, {}, {}),
-	}),
-	make_test(
-		"x + y",
-		Measurement(), {
-		make_instance("x", nil, 0, 0, true, true, true, true, {}, {}, {}),
-		make_instance("y", nil, 0, 0, true, true, true, true, {}, {}, {}),
-	}),
-	make_test(
-		"{x[1], y[2]}[3]",
-		Measurement(3), {
-		make_instance("x", nil, 0, 0, true, true, true, true, {Measurement(1)}, {}, {}),
-		make_instance("y", nil, 0, 0, true, true, true, true, {Measurement(2)}, {}, {}),
-	}),
+make_test(
+	"x",
+	Measurement(), {
+	make_instance("x", nil, nil, 0, 0, true, true, true, true, {}, {}, {}),
+}),
+make_test(
+	"x$1",
+	Measurement(), {
+	make_instance("x", nil, nil, 1, 0, true, true, true, true, {}, {}, {}),
+}),
+make_test(
+	"x$1$2",
+	Measurement(), {
+	make_instance("x", nil, nil, 1, 2, true, true, true, true, {}, {}, {}),
+}),
+make_test(
+	"{x, y}",
+	Measurement(), {
+	make_instance("x", nil, nil, 0, 0, true, true, true, true, {}, {}, {}),
+	make_instance("y", nil, nil, 0, 0, true, true, true, true, {}, {}, {}),
+}),
+make_test(
+	"x + y",
+	Measurement(), {
+	make_instance("x", nil, nil, 0, 0, true, true, true, true, {}, {}, {}),
+	make_instance("y", nil, nil, 0, 0, true, true, true, true, {}, {}, {}),
+}),
+make_test(
+	"{x[1], y[2]}[3]",
+	Measurement(3), {
+	make_instance("x", nil, nil, 0, 0, true, true, true, true, {Measurement(1)}, {}, {}),
+	make_instance("y", nil, nil, 0, 0, true, true, true, true, {Measurement(2)}, {}, {}),
+}),
 
-	make_test(
-		"{x[2], x[2ml], x[2kg]}",
-		Measurement(), {
-		make_instance("x", nil, 0, 0, true, true, true, true, {Measurement(2)}, {}, {}),
-		make_instance("x", nil, 0, 0, true, true, true, true, {Measurement(2, "ml")}, {}, {}),
-		make_instance("x", nil, 0, 0, true, true, true, true, {Measurement(2, "kg")}, {}, {}),
-	}),
-	make_test(
-		"{x[?1], x[G~1]}",
-		Measurement(), {
-		make_instance("x", nil, 0, 0, true, true, true, true, {Measurement(1, "", 0, 0, false)}, {}, {}),
-		make_instance("x", nil, 0, 0, true, true, true, true, {Measurement(1, "", 0, 0, false)}, {}, {}),
-	}),
-	make_test(
-		"{x[~~1], x[^^1]}",
-		Measurement(), {
-		make_instance("x", nil, 0, 0, true, true, true, true, {Measurement(1, "", 0, -2)}, {}, {}),
-		make_instance("x", nil, 0, 0, true, true, true, true, {Measurement(1, "", 0,  2)}, {}, {}),
-	}),
-	make_test(
-		"x[1, 2]",
-		Measurement(), {
-		make_instance("x", nil, 0, 0, true, true, true, true, {Measurement(1), Measurement(2)}, {}, {}),
-	}),
-	make_test(
-		"x[1/2g]",
-		Measurement(), {
-		make_instance("x", nil, 0, 0, true, true, true, true, {Measurement(2, "g", 1)}, {}, {}),
-	}),
+make_test(
+	"x{P1[1], P2[1]}[2]",
+	Measurement(), {
+	make_instance("x", nil, nil, 0, 0, true, true, true, true, {Measurement(2)}, {
+		make_instance("P1", nil, nil, 0, 0, true, true, true, true, {Measurement(1)}, {}, {}),
+		make_instance("P2", nil, nil, 0, 0, true, true, true, true, {Measurement(1)}, {}, {}),
+	}, {}),
+}),
 
-	make_test(
-		"{x$?, x$?$?, x$?1, x$?1$?2}",
-		Measurement(), {
-		make_instance("x", nil, 0, 0, false, true, true, true, {}, {}, {}),
-		make_instance("x", nil, 0, 0, false, false, true, true, {}, {}, {}),
-		make_instance("x", nil, 1, 0, false, true, true, true, {}, {}, {}),
-		make_instance("x", nil, 1, 2, false, false, true, true, {}, {}, {}),
-	}),
-	make_test(
-		"{x_¿, x¿}",
-		Measurement(), {
-		make_instance("x", nil, 0, 0, true, true, false, true, {}, {}, {}),
-		make_instance("x", nil, 0, 0, true, true, false, true, {}, {}, {}),
-	}),
-	make_test(
-		"?x",
-		Measurement(), {
-		make_instance("x", nil, 0, 0, true, true, true, false, {}, {}, {}),
-	}),
+make_test(
+	"{x[2], x[2ml], x[2kg]}",
+	Measurement(), {
+	make_instance("x", nil, nil, 0, 0, true, true, true, true, {Measurement(2)}, {}, {}),
+	make_instance("x", nil, nil, 0, 0, true, true, true, true, {Measurement(2, "ml")}, {}, {}),
+	make_instance("x", nil, nil, 0, 0, true, true, true, true, {Measurement(2, "kg")}, {}, {}),
+}),
+make_test(
+	"{x[?1], x[G~1]}",
+	Measurement(), {
+	make_instance("x", nil, nil, 0, 0, true, true, true, true, {Measurement(1, "", 0, 0, false)}, {}, {}),
+	make_instance("x", nil, nil, 0, 0, true, true, true, true, {Measurement(1, "", 0, 0, false)}, {}, {}),
+}),
+make_test(
+	"{x[~~1], x[^^1]}",
+	Measurement(), {
+	make_instance("x", nil, nil, 0, 0, true, true, true, true, {Measurement(1, "", 0, -2)}, {}, {}),
+	make_instance("x", nil, nil, 0, 0, true, true, true, true, {Measurement(1, "", 0,  2)}, {}, {}),
+}),
+make_test(
+	"x[1, 2]",
+	Measurement(), {
+	make_instance("x", nil, nil, 0, 0, true, true, true, true, {Measurement(1), Measurement(2)}, {}, {}),
+}),
+make_test(
+	"x[1/2g]",
+	Measurement(), {
+	make_instance("x", nil, nil, 0, 0, true, true, true, true, {Measurement(2, "g", 1)}, {}, {}),
+}),
 
-	make_test(
-		"x:m",
-		Measurement(), {
-		make_instance("x", nil, 0, 0, true, true, true, true, {}, {}, {
-			make_modifier("m", nil, nil),
-		}),
+make_test(
+	"{x$?, x$?$?, x$?1, x$?1$?2}",
+	Measurement(), {
+	make_instance("x", nil, nil, 0, 0, false, true, true, true, {}, {}, {}),
+	make_instance("x", nil, nil, 0, 0, false, false, true, true, {}, {}, {}),
+	make_instance("x", nil, nil, 1, 0, false, true, true, true, {}, {}, {}),
+	make_instance("x", nil, nil, 1, 2, false, false, true, true, {}, {}, {}),
+}),
+make_test(
+	"{x_¿, x¿}",
+	Measurement(), {
+	make_instance("x", nil, nil, 0, 0, true, true, false, true, {}, {}, {}),
+	make_instance("x", nil, nil, 0, 0, true, true, false, true, {}, {}, {}),
+}),
+make_test(
+	"?x",
+	Measurement(), {
+	make_instance("x", nil, nil, 0, 0, true, true, true, false, {}, {}, {}),
+}),
+
+make_test(
+	"x:m",
+	Measurement(), {
+	make_instance("x", nil, nil, 0, 0, true, true, true, true, {}, {}, {
+		make_modifier("m", nil, nil),
 	}),
-	make_test(
-		":m",
-		Measurement(), {
-		make_instance("", nil, 0, 0, true, true, true, true, {}, {}, {
-			make_modifier("m", nil, nil),
-		}),
+}),
+make_test(
+	":m",
+	Measurement(), {
+	make_instance("", nil, nil, 0, 0, true, true, true, true, {}, {}, {
+		make_modifier("m", nil, nil),
 	}),
+}),
+
+make_test(
+	"03T{x}",
+	Measurement(), {
+	make_instance("x", nil, "2016-01-03Z", 0, 0, true, true, true, true, {}, {}, {}),
+}),
+make_test(
+	"03T{x, y}",
+	Measurement(), {
+	make_instance("x", nil, "2016-01-03Z", 0, 0, true, true, true, true, {}, {}, {}),
+	make_instance("y", nil, "2016-01-03Z", 0, 0, true, true, true, true, {}, {}, {}),
+}),
+
+make_test(
+	"02-03T{x}",
+	Measurement(), {
+	make_instance("x", nil, "2016-02-03Z", 0, 0, true, true, true, true, {}, {}, {}),
+}),
+make_test(
+	"02-03T{x, y}",
+	Measurement(), {
+	make_instance("x", nil, "2016-02-03Z", 0, 0, true, true, true, true, {}, {}, {}),
+	make_instance("y", nil, "2016-02-03Z", 0, 0, true, true, true, true, {}, {}, {}),
+}),
+
+make_test(
+	"2015-02-03{x}",
+	Measurement(), {
+	make_instance("x", nil, "2015-02-03Z", 0, 0, true, true, true, true, {}, {}, {}),
+}),
+make_test(
+	"2015-02-03{x, y}",
+	Measurement(), {
+	make_instance("x", nil, "2015-02-03Z", 0, 0, true, true, true, true, {}, {}, {}),
+	make_instance("y", nil, "2015-02-03Z", 0, 0, true, true, true, true, {}, {}, {}),
+}),
 }
 
-function do_test(t, search_in, controllers)
+function do_test(t, scope, controllers)
 	local o = O.create(t.text)
 	U.assert(o ~= nil)
+	local text_rewrite = O.write_text_string(o, true)
+	U.print("%s  =>", text_rewrite)
 
-	local comp = Composition(o, search_in, controllers)
+	local comp = Composition()
+	U.assert(comp:from_object(o, scope, controllers) or not t.comp)
 	check_composition_equal(comp, t.comp)
-
-	comp:to_object(o)
-	local text = O.write_text_string(o, #comp.items > 1)
-	if #comp.items > 1 then
-		if comp.measurement:is_empty() then
-			text = "{" .. string.sub(text, 4, -3) .. "}"
-		else
-			local bi
-			local i = 0
-			while i ~= nil do
-				bi = i
-				i = string.find(text, "%[", i + 1)
-			end
-			text = "{" .. string.sub(text, 4, bi - 3) .. "}" .. string.sub(text, bi)
-		end
+	if t.comp then
+		check_composition_equal(comp, t.comp)
+		comp:to_object(o)
+		U.print("%s", O.write_text_string(o, true))
+	else
+		U.print("(expected)")
 	end
-	text = string.gsub(text, "\t", " ")
-	text = string.gsub(text, "\n", ",")
-	U.print("%s  =>\n%s", t.text, text)
 end
 
 function main()
-	local search_in = {}
+	local scope = make_time("2016-01-01Z")
 	local controllers = {}
 	for _, t in pairs(translation_tests) do
-		do_test(t, search_in, controllers)
+		do_test(t, scope, controllers)
 	end
 
 	return 0

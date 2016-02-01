9c1ed4e049bf952c1fbb8ffb19e5592e8eccd84d
Coranna Howard <me@komiga.com>
2016-01-31 20:04:12 -0500

    lib/core/directory/Director.lua: implemented basic interface.

--- lib/core/src/quanta/core/director/Director.lua
+++ lib/core/src/quanta/core/director/Director.lua
@@ -6,14 +6,43 @@ local Match = require "Quanta.Match"
 local Tracker = require "Quanta.Tracker"
 local M = U.module(...)
 
+M.debug = false
+
 U.class(M)
 
 function M:__init()
-	self.action_templates = {}
+	self.action_classes = {}
 end
 
-function M:read_action(context, into, obj)
-	-- TODO
+function M:read_action(context, entry, action, obj)
+	local action_class
+	local registered = self.action_classes[action.id_hash]
+	if not registered then
+		action_class = Tracker.UnknownAction
+	else
+		action_class = registered.class
+		if M.debug then
+			U.assert(action.id == registered.id)
+		end
+	end
+
+	action.data = action_class()
+	return action.data:from_object(context, entry, action, obj)
+end
+
+function M:register(id, class)
+	U.type_assert(id, "string")
+	U.assert(U.is_functable(class))
+
+	local id_hash = O.hash_name(id)
+	U.assert(id_hash ~= O.NAME_NULL, "action id must be non-empty")
+	U.assert(not self.action_classes[id], "action '%s' is already registered", id)
+
+	self.action_classes[id_hash] = {
+		id = id,
+		id_hash = id_hash,
+		class = class,
+	}
 end
 
 return M

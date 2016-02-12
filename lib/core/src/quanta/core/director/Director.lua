6ca71b7de27bb96490e019d7d1faa1ea77ece2c0
Coranna Howard <me@komiga.com>
2016-02-11 19:45:14 -0500

    lib/core/director/Director.lua: added modifier class interface.

--- lib/core/src/quanta/core/director/Director.lua
+++ lib/core/src/quanta/core/director/Director.lua
@@ -3,6 +3,7 @@ u8R""__RAW_STRING__(
 local U = require "togo.utility"
 local O = require "Quanta.Object"
 local Match = require "Quanta.Match"
+local Instance = require "Quanta.Instance"
 local Tracker = require "Quanta.Tracker"
 local M = U.module(...)
 
@@ -11,38 +12,55 @@ M.debug = false
 U.class(M)
 
 function M:__init()
+	self.modifier_classes = {}
 	self.action_classes = {}
 end
 
-function M:read_action(context, entry, action, obj)
-	local action_class
-	local registered = self.action_classes[action.id_hash]
-	if not registered then
-		action_class = Tracker.UnknownAction
-	else
-		action_class = registered.class
-		if M.debug then
-			U.assert(action.id == registered.id)
-		end
-	end
-
-	action.data = action_class()
-	return action.data:from_object(context, entry, action, obj)
-end
-
-function M:register(id, class)
+local function register(bucket, name, id, class)
 	U.type_assert(id, "string")
 	U.assert(U.is_functable(class))
 
 	local id_hash = O.hash_name(id)
-	U.assert(id_hash ~= O.NAME_NULL, "action id must be non-empty")
-	U.assert(not self.action_classes[id], "action '%s' is already registered", id)
+	U.assert(id_hash ~= O.NAME_NULL, "%s id must be non-empty", kind)
+	U.assert(not bucket[id_hash], "%s '%s' is already registered", kind, id)
 
-	self.action_classes[id_hash] = {
+	bucket[id_hash] = {
 		id = id,
 		id_hash = id_hash,
 		class = class,
 	}
+end
+
+function M:register_modifier(id, class)
+	register(self.modifier_classes, "modifier", id, class)
+end
+
+function M:register_action(id, class)
+	register(self.action_classes, "action", id, class)
+end
+
+local function read(bucket, unknown_class, context, parent, holder, obj)
+	local class
+	local registered = bucket[holder.id_hash]
+	if not registered then
+		class = unknown_class
+	else
+		class = registered.class
+		if M.debug then
+			U.assert(holder.id == registered.id)
+		end
+	end
+
+	holder.data = class()
+	return holder.data:from_object(context, parent, holder, obj)
+end
+
+function M:read_modifier(context, instance, modifier, obj)
+	return read(self.modifier_classes, Instance.UnknownModifier, context, instance, modifier, obj)
+end
+
+function M:read_action(context, entry, action, obj)
+	return read(self.action_classes, Tracker.UnknownAction, context, entry, action, obj)
 end
 
 return M

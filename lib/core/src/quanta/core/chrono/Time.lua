4366aa40bd0d21f2d060a91b79ed9a7d4bc150c3
Coranna Howard <me@komiga.com>
2016-02-15 13:44:52 -0500

    lib/core/chrono/Time.lua: added some constants.

--- lib/core/src/quanta/core/chrono/Time.lua
+++ lib/core/src/quanta/core/chrono/Time.lua
@@ -5,6 +5,11 @@ local M = U.module(...)
 
 M.debug = false
 
+M.SECS_PER_MINUTE = 60
+M.SECS_PER_HOUR = 60 * 60
+M.SECS_PER_DAY = 24 * 60 * 60
+M.SECS_PER_WEEK = 7 * M.SECS_PER_DAY
+
 function M.__module_init__()
 	U.set_functable(M, function(_, ...)
 		return M.__mm_ctor(...)
